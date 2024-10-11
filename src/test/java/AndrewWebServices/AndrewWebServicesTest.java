5a6012df6d8fdf69d7c2f0a85ef9ff0376434de9
number000000 <maggieliulmh@gmail.com>
2024-10-11 01:04:49 -0400

    Created a stub for recsys and mock the promoservice

--- src/test/java/AndrewWebServices/AndrewWebServicesTest.java
+++ src/test/java/AndrewWebServices/AndrewWebServicesTest.java
@@ -6,12 +6,16 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.mockito.Mockito.*;
+
 public class AndrewWebServicesTest {
     Database database;
     InMemoryDatabase fakeDatabase;
     RecSys recommender;
     PromoService promoService;
     AndrewWebServices andrewWebService;
+    RecSys mockRecSys;
+    PromoService mockPromoService;
 
     @Before
     public void setUp() {
@@ -21,8 +25,11 @@ public class AndrewWebServicesTest {
         recommender = new RecSys();
         promoService = new PromoService();
 
+        mockRecSys = mock(RecSys.class);
+        mockPromoService = mock(PromoService.class);
+        
         //andrewWebService = new AndrewWebServices(database, recommender, promoService);
-        andrewWebService = new AndrewWebServices(fakeDatabase, recommender, promoService);
+        andrewWebService = new AndrewWebServices(fakeDatabase, mockRecSys, mockPromoService);
     }
 
     @Test
@@ -35,18 +42,32 @@ public class AndrewWebServicesTest {
     @Test
     public void testGetRecommendation() {
         // This is taking way too long to test
-        assertEquals("Animal House", andrewWebService.getRecommendation("Scotty"));
+        //assertEquals("Animal House", andrewWebService.getRecommendation("Scotty"));
+
+        when(mockRecSys.getRecommendation("Scotty")).thenReturn("Animal House");
+        String result = andrewWebService.getRecommendation("Scotty");
+
+        verify(mockRecSys).getRecommendation("Scotty");
+        assertEquals("Animal House", result);
     }
 
     @Test
     public void testSendEmail() {
         // How should we test sendEmail() when it doesn't have a return value?
         // Hint: is there something from Mockito that seems useful here?
+        String fakeEmail = "fake email.";
+        andrewWebService.sendPromoEmail(fakeEmail);
+
+        verify(mockPromoService).mailTo(fakeEmail);
     }
 
     @Test
     public void testNoSendEmail() {
         // How should we test that no email has been sent in certain situations (like right after logging in)?
         // Hint: is there something from Mockito that seems useful here?
+        String fakeEmail = "fake email.";
+        andrewWebService.logIn("Scotty", 17214);
+
+        verify(mockPromoService, never()).mailTo(fakeEmail);
     }
 }
